ed8f25c001b4e0e8ba78bec3cecb32c0f042fc0d
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-03 20:38:54 +0300

    implementing delete method

--- src/main/java/com/TodaApp/repository/ItemRepository.java
+++ src/main/java/com/TodaApp/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package com.TodaApp.repository;
 import com.TodaApp.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    Item findByTitle(String title);
+    Optional<Item> findById(int id);
+    Optional<Item> deleteById(int id);
 }
