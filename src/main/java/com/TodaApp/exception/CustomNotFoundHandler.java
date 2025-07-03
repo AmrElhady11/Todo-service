721b59653056cf5c21cb5d68c0a0ea6172b6dbd9
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-03 20:33:34 +0300

    creating custom error handling for not found requests

--- src/main/java/com/TodaApp/exception/CustomNotFoundHandler.java
+++ src/main/java/com/TodaApp/exception/CustomNotFoundHandler.java
@@ -1,20 +1,22 @@
 package com.TodaApp.exception;
 
+import com.TodaApp.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomNotFoundHandler {
 
-//    @ExceptionHandler(UserNotFoundException.class)
-//    public ResponseEntity<UserErrorResponse> handleProductNotFound(UserNotFoundException ex){
-//        UserErrorResponse error = new UserErrorResponse();
-//        error.setMessage(ex.getMessage());
-//        error.setCode(HttpStatus.NOT_FOUND.value());
-//        error.setTimestamp(System.currentTimeMillis());
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//
-//    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException ex){
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getMessage());
+        error.setCode(HttpStatus.NOT_FOUND.value());
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+
 }
